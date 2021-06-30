4720857456279e1a6e792996a6355ec106f906f6
Eliana-Abate <eliana.abate@outlook.it>
2021-06-30 16:09:55 +0200

    added html elements and js var

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* STEP:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+consigli:
+utilizziamo il console.log per verificare che il foglio JS sia connesso correttamente;
+Analizziamo pensando "in italiano" agli step che dobbiamo affrontare
+sfruttiamo sempre il console.log man mano che procediamo negli step per assicurarci che fino a quel punto tutto funziona come vorremmo 
+*/
+
+var firstName = prompt('Scrivi il tuo nome');
+console.log(firstName); 
+
+var lastName = prompt('Scrivi il tuo cognome');
+console.log(lastName);
+
+var favouriteColor = ('Scrivi il tuo colore preferito');
