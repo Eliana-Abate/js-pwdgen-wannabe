f5122e5392774551823b1b6a6d0d1c963ce4c1e3
Eliana-Abate <eliana.abate@outlook.it>
2021-06-30 16:57:00 +0200

    corrected value of var number

--- js/script.js
+++ js/script.js
@@ -2,11 +2,7 @@
 Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
-Infine scrivi sulla pagina nomecognomecolorepreferito21
-consigli:
-utilizziamo il console.log per verificare che il foglio JS sia connesso correttamente;
-Analizziamo pensando "in italiano" agli step che dobbiamo affrontare
-sfruttiamo sempre il console.log man mano che procediamo negli step per assicurarci che fino a quel punto tutto funziona come vorremmo 
+Infine scrivi sulla pagina nomecognomecolorepreferito21 
 */
 
 var firstName = prompt('Scrivi il tuo nome');
@@ -18,9 +14,9 @@ console.log(lastName);
 var favouriteColor = prompt('Scrivi il tuo colore preferito');
 console.log(favouriteColor);
 
-var number = 21
+var number = "21";
 
 var yourPassword = document.getElementById('password');
 console.log(yourPassword);
 
-yourPassword.innerHTML = firstName + lastName + favouriteColor + number
+yourPassword.innerHTML = firstName + lastName + favouriteColor + number;
